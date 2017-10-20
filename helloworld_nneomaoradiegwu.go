e3e71557b19d1089d7878f553ce1334d85c6248a
noradiegwu <nneomaoradiegwu2019@u.northwestern.edu>
2017-10-20 04:45:06 -0500

    hello world in golang

---
+++ helloworld_nneomaoradiegwu.go
@@ -0,0 +1,15 @@
+// https://github.com/AliceWonderland/hacktoberfest/issues/162
+
+// LANGUAGE: GoLang
+// ENV: Sublime
+// AUTHOR: Nneoma Oradiegwu
+// GITHUB: https://github.com/noradiegwu
+
+package main
+
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!");
+}
